83c15983cbf72b0fb3051205d6f7cf816f073293
Marcolux <mccolucci1@gmail.com>
2024-03-04 05:47:07 -0600

    set up navigation bar and pages

--- chef-kapernaros/script/main.js
+++ chef-kapernaros/script/main.js
@@ -1,2 +1,35 @@
 console.log('hello world');
-console.log('all the time');
+var hambMenu = document.querySelector('.hamburger-menu');
+var navMenu = document.getElementById('navMenu');
+var navBar = document.querySelector('.navBar');
+var spanToBreak = document.querySelectorAll('.brSm');
+hambMenu.addEventListener('click', function () {
+    navMenu.classList.toggle('show');
+    navMenu.classList.toggle('hide', !navMenu.classList.contains('show'));
+    hambMenu.classList.toggle('openHam');
+    navBar.classList.toggle('expand');
+});
+var navBarAdjToScreen = function () {
+    if (window.innerWidth < 505 && navBar.classList.contains('biggerScreen')) {
+        navBar.classList.add('mobileView');
+        navBar.classList.remove('biggerScreen');
+        spanToBreak.forEach(function (el) {
+            if (!el.firstChild || el.firstChild.nodeName !== 'BR') {
+                var brEl = document.createElement('br');
+                el.prepend(brEl);
+            }
+        });
+    }
+    else if (window.innerWidth >= 505 && !navBar.classList.contains('biggerScreen')) {
+        navBar.classList.remove('mobileView');
+        navBar.classList.add('biggerScreen');
+        spanToBreak.forEach(function (el) {
+            if (el.firstChild && el.firstChild.nodeName === 'BR') {
+                el.removeChild(el.firstChild);
+            }
+        });
+    }
+};
+window.addEventListener('resize', navBarAdjToScreen);
+navBarAdjToScreen();
+console.log('all the time 4 try');
--- chef-kapernaros/script/main.ts
+++ chef-kapernaros/script/main.ts
@@ -1,2 +1,42 @@
 console.log('hello world')
-console.log('all the time')
+
+const hambMenu = document.querySelector('.hamburger-menu') as HTMLElement
+const navMenu = document.getElementById('navMenu') as HTMLElement
+const navBar = document.querySelector('.navBar') as HTMLElement
+const spanToBreak = document.querySelectorAll('.brSm') as NodeListOf<HTMLElement>
+
+hambMenu.addEventListener('click', function() {
+    navMenu.classList.toggle('show')
+    navMenu.classList.toggle('hide',!navMenu.classList.contains('show'))
+    hambMenu.classList.toggle('openHam') 
+    navBar.classList.toggle('expand')
+})
+
+const navBarAdjToScreen = () => {
+    
+    if (window.innerWidth < 505 && navBar.classList.contains('biggerScreen')) {
+       
+        navBar.classList.add('mobileView')
+        navBar.classList.remove('biggerScreen')
+        spanToBreak.forEach(el => {
+            if (!el.firstChild || el.firstChild.nodeName !== 'BR') {
+                const brEl = document.createElement('br')
+                el.prepend(brEl)
+            }
+        })
+    } else if (window.innerWidth >= 505 && !navBar.classList.contains('biggerScreen')) {
+        
+        navBar.classList.remove('mobileView')
+        navBar.classList.add('biggerScreen')
+        spanToBreak.forEach(el => {
+            if (el.firstChild && el.firstChild.nodeName === 'BR') {
+                el.removeChild(el.firstChild)
+            }
+        })
+    }
+}
+
+window.addEventListener('resize',navBarAdjToScreen)
+navBarAdjToScreen()
+
+console.log('all the time 4 try')
