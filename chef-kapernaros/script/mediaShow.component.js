26fd88227b6e040d8158b2e008cea99e848805b8
Marcolux <mccolucci1@gmail.com>
2024-05-01 22:46:27 -0500

    Added all the pictures on media,added left right scroll with keyboard

--- chef-kapernaros/script/mediaShow.component.js
+++ chef-kapernaros/script/mediaShow.component.js
@@ -56,13 +56,13 @@ function initPicCollection(newPicturesList, collection) {
     function showPicture(index) {
         var picture = newPicturesList.getSinglePic(index);
         if (index < newPicturesList.getLastIndex() && index !== 0) {
-            carouselElement.innerHTML = "\n                <div id=\"picShowing\" class=\"flex flex-alignItems-center\">\n                    <button class=\"p-20\" id=\"prevPicture\">\n                        <i class=\"fa-solid fa-chevron-left fontSize40\"></i>\n                    </button>\n                    <img class=\"picToEn\" src=\"".concat(picture.src, "\">\n                    <button class=\"p-20\" id=\"nextPicture\">\n                        <i class=\"fa-solid fa-chevron-right fontSize40\"></i>\n                    </button>\n                </div>\n            ");
+            carouselElement.innerHTML = "\n                <div id=\"picShowing\" class=\"flex flex-alignItems-center\">\n                    <button class=\"p-20\" id=\"prevPicture\">\n                        <i class=\"fa-solid fa-chevron-left fontSize40\"></i>\n                    </button>\n                    <div class=\"flex flex-justifyContent-center flex-alignItems-center\" style=\"width: 500px; height: 66vh; background-color: #FFF\">\n                        <img class=\"picToEn\" src=\"".concat(picture.src, "\">\n                    </div>\n                    <button class=\"p-20\" id=\"nextPicture\">\n                        <i class=\"fa-solid fa-chevron-right fontSize40\"></i>\n                    </button>\n                </div>\n            ");
         }
         else if (index === newPicturesList.getLastIndex()) {
-            carouselElement.innerHTML = "\n                <div id=\"picShowing\" class=\"flex flex-alignItems-center\">\n                    <button class=\"p-20\" id=\"prevPicture\">\n                        <i class=\"fa-solid fa-chevron-left fontSize40\"></i>\n                    </button>\n                    <img class=\"picToEn\" src=\"".concat(picture.src, "\">\n\n                    <button class=\"p-20\" id=\"nextPicture\"></button>\n                </div>\n            ");
+            carouselElement.innerHTML = "\n                <div id=\"picShowing\" class=\"flex flex-alignItems-center\">\n                    <button class=\"p-20\" id=\"prevPicture\">\n                        <i class=\"fa-solid fa-chevron-left fontSize40\"></i>\n                    </button>\n                    <div class=\"flex flex-justifyContent-center flex-alignItems-center\" style=\"width: 500px; height: 66vh; background-color: #FFF\">\n                        <img class=\"picToEn\" src=\"".concat(picture.src, "\">\n                    </div>\n\n                    <button class=\"p-20\" id=\"nextPicture\"></button>\n                </div>\n            ");
         }
         else if (index === 0) {
-            carouselElement.innerHTML = "\n                <div id=\"picShowing\" class=\"flex flex-alignItems-center\">\n                    <button class=\"p-20\" id=\"prevPicture\"></button>\n\n                    <img class=\"picToEn\" src=\"".concat(picture.src, "\">\n                    <button class=\"p-20\" id=\"nextPicture\">\n                        <i class=\"fa-solid fa-chevron-right fontSize40\"></i>\n                    </button>\n                </div>\n            ");
+            carouselElement.innerHTML = "\n                <div id=\"picShowing\" class=\"flex flex-alignItems-center\">\n                    <button class=\"p-20\" id=\"prevPicture\"></button>\n\n                    <div class=\"flex flex-justifyContent-center flex-alignItems-center\" style=\"width: 500px; height: 66vh; background-color: #FFF\">\n                        <img class=\"picToEn\" src=\"".concat(picture.src, "\">\n                    </div>\n                    <button class=\"p-20\" id=\"nextPicture\">\n                        <i class=\"fa-solid fa-chevron-right fontSize40\"></i>\n                    </button>\n                </div>\n            ");
         }
         setTimeout(function () {
             var imgs = carouselElement.querySelector('img');
@@ -77,7 +77,7 @@ function initPicCollection(newPicturesList, collection) {
                     imgShowing.classList.remove('fullscreen-mode');
                 }
             });
-        }, 350);
+        }, 150);
         var prevButton = document.getElementById('prevPicture');
         var nextButton = document.getElementById('nextPicture');
         if (prevButton) {
@@ -102,6 +102,14 @@ function initPicCollection(newPicturesList, collection) {
         }
         showPicture(currentPictureIndex);
     }
+    document.addEventListener('keydown', function (event) {
+        if (event.key === "ArrowLeft") { // Left arrow key
+            movePicture(-1);
+        }
+        else if (event.key === "ArrowRight") { // Right arrow key
+            movePicture(1);
+        }
+    });
     collection.unshift(first);
     console.log('carousel running 1');
 }
@@ -135,44 +143,94 @@ document.addEventListener('DOMContentLoaded', function () {
         { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714607629/Niko/f_f_12_lw4pkp.webp" },
         { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714607919/Niko/f_f_13_njsqqk.webp" },
         { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714608753/Niko/f_f_14_dulpgp.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714609169/Niko/IMG_20220409_014025_756_hdxn1q.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714609175/Niko/IMG_20230427_014541_976_v4l2dq.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714609182/Niko/IMG_20230427_014541_962_jjyubs.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714609188/Niko/IMG_20211013_232202_678_mba28l.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714609195/Niko/IMG_20230427_014542_012_mgyxej.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714609202/Niko/chef-kapernaros.pic1_kfa6q4.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714609208/Niko/0-02-01-d1cbb862e95655f9de70c014b3d5de577f5f7d7e254eb68fd8faac49e4117cc7_32f11885e251badf_pnyuja.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714609216/Niko/0-02-01-da1f1307d893546b9ff70b3a7ff23d99510b9fc61b81d29b80b1f83c2aa6a2ea_21ef0346a984fe60_fjxmk3.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714609224/Niko/IMG_20211030_233514_527_gcowcm.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714609231/Niko/IMG_20190619_110847_219_uaeq7y.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714609238/Niko/IMG_20230427_094628_086_tluaxx.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714609247/Niko/IMG_20220506_140708_077_vs0ipp.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714609255/Niko/IMG_20220716_124026_531_i7ojsm.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714609260/Niko/IMG_20230313_231738_057_cc62mi.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714609268/Niko/IMG_20231015_235042_366_lc5jdz.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714609282/Niko/IMG_20230427_014542_180_sr3c1i.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714609289/Niko/IMG_20231015_235041_946_spkz2f.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714609298/Niko/IMG_20230430_093533_025_s1tzgr.webp" },
-        { src: "AAAAAA" },
-        { src: "AAAAAA" },
-        { src: "AAAAAA" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714610871/Niko/f_f_15_syo3bh.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714611082/Niko/f_f_16_i861kj.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714611192/Niko/f_f_17_dhcths.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714611315/Niko/f_f_18_nwuxra.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714611418/Niko/f_f_19_cygdn0.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714611504/Niko/f_f_20_yxq3gm.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714611614/Niko/f_f_21_n0jrv2.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714611684/Niko/f_f_22_agudmu.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714611853/Niko/f_f_23_zgxxng.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714611927/Niko/f_f_24_bl55y4.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714612087/Niko/f_f_25_oai4zu.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714612235/Niko/f_f_26_oi8rbi.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714612392/Niko/f_f_27_uzpolq.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714612584/Niko/f_f_28_uvsrep.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714612645/Niko/f_f_29_rznm8c.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714612758/Niko/f_f_30_le2dkm.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714612846/Niko/f_f_31_hetsw8.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714612983/Niko/f_f_32_jvdutb.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714613151/Niko/f_f_33_ke48sd.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714613141/Niko/f_f_34_zcya8h.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714613295/Niko/f_f_35_zgjzm4.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714613366/Niko/f_f_36_j484sl.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714613623/Niko/f_f_37_pxhi3p.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714613639/Niko/f_f_38_mfwtlr.webp" },
     ];
     var growingInGreeceList_src = [
         { src: 'growingInGreeceList' },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714588368/viber_image_2024-04-03_18-24-05-958_exnogl.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714588373/viber_image_2024-04-03_18-24-06-061_vu6r6n.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714588359/chef-kapernaros.growing-3_qky1ka.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714588359/chef-kapernaros.growing-2_ynixxn.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714588359/chef-kapernaros.growing-1_wuqge2.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714588367/viber_image_2024-04-03_18-24-05-864_y2iddp.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714588368/viber_image_2024-04-03_18-24-05-883_khfhow.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714588360/DSC_0832_pdjreg.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714588360/received_10154563095763189_jtd9qj.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714588360/received_1283942424950699_lqddle.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714588360/DSC_0664_hhhg9j.webp" },
-        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714588360/FB_IMG_1586845370822_fxplne.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714614499/Niko/niko-growing%20greece/g_g_5_qva7un.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714615983/Niko/niko-growing%20greece/g_g_10_h4dbvx.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714616134/Niko/niko-growing%20greece/g_g_11_sxxf3p.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714614491/Niko/niko-growing%20greece/g_g_4_peo48x.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714615869/Niko/niko-growing%20greece/g_g_9_dxdqpz.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714616225/Niko/niko-growing%20greece/g_g_12_nw6vmq.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714615676/Niko/niko-growing%20greece/g_g_8_xaopxm.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714615465/Niko/niko-growing%20greece/g_g_7_tyzfaa.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714615279/Niko/niko-growing%20greece/g_g_6_vtni3k.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714614484/Niko/niko-growing%20greece/g_g_3_xbjxxr.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/w_1100,h_1000,c_fill/v1714614217/Niko/niko-growing%20greece/g_g_1_r7i5t5.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/w_1300,h_900,c_fill/v1714614224/Niko/niko-growing%20greece/g_g_2_myfflq.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714616486/Niko/niko-growing%20greece/g_g_14_jcxgcs.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714616872/Niko/niko-growing%20greece/g_g_15_o0hg55.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714617566/Niko/niko-growing%20greece/g_g_16_jrjtzg.webp" },
     ];
-    var allSources = [friendsCelebList_src, growingInGreeceList_src];
+    var charityEventsList_src = [
+        { src: "charityEventsList" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714618542/Niko/charity%20events/c_e_1_nnx7ps.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714618551/Niko/charity%20events/c_e_2_wnpmve.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714618559/Niko/charity%20events/c_e_3_wup5of.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714618568/Niko/charity%20events/c_e_4_lxljgw.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714618577/Niko/charity%20events/c_e_5_ybs4do.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714618586/Niko/charity%20events/c_e_6_jgwge1.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714619085/Niko/charity%20events/c_e_7_jvuw9w.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714618606/Niko/charity%20events/c_e_8_wskun4.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714618751/Niko/charity%20events/c_e_9_afuwys.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714618761/Niko/charity%20events/c_e_10_v4gaep.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714618771/Niko/charity%20events/c_e_11_ffbduo.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714618781/Niko/charity%20events/c_e_12_pdspri.webp" },
+    ];
+    var competitionShows_src = [
+        { src: "competitionShows" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714619451/Niko/competitions/c_s_1_nzov2c.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714619460/Niko/competitions/c_s_2_tfjvel.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714619469/Niko/competitions/c_s_3_etu3kj.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714619478/Niko/competitions/c_s_4_rptc6s.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714619771/Niko/competitions/c_s_5_qhlsa1.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714619828/Niko/competitions/c_s_6_qbbqox.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714619837/Niko/competitions/c_s_8_a7ucnk.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714619847/Niko/competitions/c_s_9_a90wfg.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714619857/Niko/competitions/c_s_10_cgzqss.webp" }
+    ];
+    var onThePass_src = [
+        { src: "onThePass" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714620128/Niko/on%20the%20pass/o_t_p_4_efmsmz.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714620119/Niko/on%20the%20pass/o_t_p_3_wnefgs.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714620109/Niko/on%20the%20pass/o_t_p_2_q4hmvq.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714620100/Niko/on%20the%20pass/o_t_p_1_va8hpk.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714620341/Niko/on%20the%20pass/o_t_p_5_z9bvsq.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714620351/Niko/on%20the%20pass/o_t_p_6_abdl2b.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714620361/Niko/on%20the%20pass/o_t_p_7_xtkrv7.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714620371/Niko/on%20the%20pass/o_t_p_8_f5uqa0.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714620536/Niko/on%20the%20pass/o_t_p_9_babbqd.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714620546/Niko/on%20the%20pass/o_t_p_10_k8dkae.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714620557/Niko/on%20the%20pass/o_t_p_11_spnsd6.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714620568/Niko/on%20the%20pass/o_t_p_12_mglkcu.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714620784/Niko/on%20the%20pass/o_t_p_13_bs7c0y.webp" },
+        { src: "https://res.cloudinary.com/drdrs6pdq/image/upload/v1714620795/Niko/on%20the%20pass/o_t_p_14_f2im1y.webp" },
+    ];
+    var allSources = [friendsCelebList_src, growingInGreeceList_src, charityEventsList_src, competitionShows_src, onThePass_src];
     var allNotActiveTi = document.querySelectorAll('.secTitles');
     allNotActiveTi.forEach(function (listEl) {
         listEl.addEventListener('click', function () {
