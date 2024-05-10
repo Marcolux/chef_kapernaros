ffd852883c85f065b3cd17ed42ae9ce6bb5cb524
Marcolux <mccolucci1@gmail.com>
2024-05-10 16:51:43 -0500

    Coming soon css

--- chef-kapernaros/script/main.js
+++ chef-kapernaros/script/main.js
@@ -1,109 +1,105 @@
-"use strict";
-var _a;
-console.log('hello world');
-const hambMenu = document.querySelector('.hamburger-menu');
-const navMenu = document.getElementById('navMenu');
-const navBar = document.querySelector('.navBar');
-const spanToBreak = document.querySelectorAll('.brSm');
-hambMenu.addEventListener('click', function () {
-    navMenu.classList.toggle('show');
-    navMenu.classList.toggle('hide', !navMenu.classList.contains('show'));
-    hambMenu.classList.toggle('openHam');
-    navBar.classList.toggle('expand');
-});
-const navBarAdjToScreen = () => {
-    if (window.innerWidth < 750 && navBar.classList.contains('biggerScreen')) {
-        navBar.classList.add('mobileView');
-        navBar.classList.remove('biggerScreen');
-        spanToBreak.forEach(el => {
-            if (!el.firstChild || el.firstChild.nodeName !== 'BR') {
-                const brEl = document.createElement('br');
-                el.prepend(brEl);
-            }
-        });
-    }
-    else if (window.innerWidth >= 750 && !navBar.classList.contains('biggerScreen')) {
-        navBar.classList.remove('mobileView');
-        navBar.classList.add('biggerScreen');
-        spanToBreak.forEach(el => {
-            if (el.firstChild && el.firstChild.nodeName === 'BR') {
-                el.removeChild(el.firstChild);
-            }
-        });
-    }
-};
-const navScrolling = () => {
-    if (window.pageYOffset > 30) {
-        navBar.classList.add('scrolled');
-    }
-    else {
-        if (!navBar.classList.contains('scrolledAlways')) {
-            navBar.classList.remove('scrolled');
-        }
-    }
-};
-const picAnimation = () => {
-    document.querySelectorAll('.bioLandingPicContainer img').forEach(img => {
-        if (window.pageYOffset >= 600) {
-            img.classList.add('in-view');
-        }
-        else if (window.pageYOffset < 500) {
-            img.classList.remove('in-view');
-        }
-    });
-    document.querySelectorAll('.achCards').forEach(img => {
-        if (window.pageYOffset > 1250) {
-            img.classList.add('in-view');
-        }
-        else if (window.pageYOffset < 1150) {
-            img.classList.remove('in-view');
-        }
-    });
-    document.querySelectorAll('.charityLandingPicContainer img').forEach(img => {
-        if (window.pageYOffset >= 1950) {
-            img.classList.add('in-view');
-        }
-        else if (window.pageYOffset < 1850) {
-            img.classList.remove('in-view');
-        }
-    });
-};
-window.addEventListener('resize', navBarAdjToScreen);
-navBarAdjToScreen();
-window.addEventListener('scroll', () => {
-    navScrolling();
-    picAnimation();
-});
-navScrolling();
-picAnimation();
-const container = document.querySelector('#bigPic');
-let allNotActiveTitles = document.querySelectorAll('.secTitles');
-allNotActiveTitles.forEach(listEl => {
-    listEl.addEventListener('click', () => {
-        allNotActiveTitles = document.querySelectorAll('.secTitles');
-        console.log(listEl);
-        if (!listEl.classList.contains('active')) {
-            const titleActive = [...allNotActiveTitles].filter(el => el.classList.contains('active'))[0];
-            titleActive.classList.remove('active');
-            listEl.classList.add('active');
-        }
-    });
-});
-emailjs.init('0wA6kpUaumn2FNdbg');
-const messageSent = document.querySelector('#messageSent');
-const inputText = document.querySelectorAll('.inputText');
-(_a = document.getElementById('myForm')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', function (event) {
-    event.preventDefault(); // Prevent the default form submission
-    emailjs.sendForm('service_m5a5vcb', 'template_1jfyvjh', this)
-        .then(function (response) {
-        messageSent.classList.remove('hide');
-        inputText.forEach(text => text.value = '');
-        console.log('SUCCESS!', response.status, response.text);
-        setTimeout(() => { messageSent.classList.add('hide'); }, 5000);
-        // alert("Email sent successfully!")
-    }, function (error) {
-        console.log('FAILED...', error);
-        alert("Failed to send email.");
-    });
-});
-console.log('all the time 4 try');
+// console.log('hello world')
+// const hambMenu = document.querySelector('.hamburger-menu') as HTMLElement
+// const navMenu = document.getElementById('navMenu') as HTMLElement
+// const navBar = document.querySelector('.navBar') as HTMLElement
+// const spanToBreak = document.querySelectorAll('.brSm') as NodeListOf<HTMLElement>
+// if (hambMenu) {
+//     hambMenu.addEventListener('click', function() {
+//         navMenu.classList.toggle('show')
+//         navMenu.classList.toggle('hide',!navMenu.classList.contains('show'))
+//         hambMenu.classList.toggle('openHam') 
+//         navBar.classList.toggle('expand')
+//     })
+// }
+// const navBarAdjToScreen = () => {
+//     if (window.innerWidth < 750 && navBar.classList.contains('biggerScreen')) {
+//         navBar.classList.add('mobileView')
+//         navBar.classList.remove('biggerScreen')
+//         spanToBreak.forEach(el => {
+//             if (!el.firstChild || el.firstChild.nodeName !== 'BR') {
+//                 const brEl = document.createElement('br')
+//                 el.prepend(brEl)
+//             }
+//         })
+//     } else if (window.innerWidth >= 750 && !navBar.classList.contains('biggerScreen')) {
+//         navBar.classList.remove('mobileView')
+//         navBar.classList.add('biggerScreen')
+//         spanToBreak.forEach(el => {
+//             if (el.firstChild && el.firstChild.nodeName === 'BR') {
+//                 el.removeChild(el.firstChild)
+//             }
+//         })
+//     }
+// }
+// const navScrolling = ()=>{
+//     if (window.pageYOffset > 30) {
+//         navBar.classList.add('scrolled')
+//     } else {
+//         if (!navBar.classList.contains('scrolledAlways')) {
+//             navBar.classList.remove('scrolled')
+//         }
+//     }
+// }
+// const picAnimation = () => {
+//     document.querySelectorAll('.bioLandingPicContainer img').forEach(img => {
+//         if (window.pageYOffset >= 600) {
+//             img.classList.add('in-view')
+//         } else if (window.pageYOffset < 500) {
+//             img.classList.remove('in-view')
+//         }
+//     })
+//     document.querySelectorAll('.achCards').forEach(img => {
+//         if (window.pageYOffset > 1250) {
+//             img.classList.add('in-view')
+//         } else if (window.pageYOffset < 1150) {
+//             img.classList.remove('in-view')
+//         }
+//     })
+//     document.querySelectorAll('.charityLandingPicContainer img').forEach(img => {
+//         if (window.pageYOffset >= 1950) {
+//             img.classList.add('in-view')
+//         } else if (window.pageYOffset < 1850) {
+//             img.classList.remove('in-view')
+//         }
+//     })
+// }
+// window.addEventListener('resize',navBarAdjToScreen)
+// navBarAdjToScreen()
+// window.addEventListener('scroll',() => { 
+//     navScrolling() 
+//     picAnimation()
+// })
+// navScrolling()
+// picAnimation()
+// const container = document.querySelector('#bigPic') as HTMLDivElement
+// let allNotActiveTitles = document.querySelectorAll('.secTitles') as NodeListOf<HTMLLIElement>
+// allNotActiveTitles.forEach( listEl => {
+//     listEl.addEventListener('click', () => {
+//         allNotActiveTitles = document.querySelectorAll('.secTitles') as NodeListOf<HTMLLIElement>
+//         console.log(listEl)
+//         if (!listEl.classList.contains('active')) {
+//             const titleActive = [...allNotActiveTitles].filter(el => el.classList.contains('active'))[0]
+//             titleActive.classList.remove('active')
+//             listEl.classList.add('active')
+//         }
+//     })
+// })
+// declare var emailjs: any;
+// emailjs.init('0wA6kpUaumn2FNdbg')
+// const messageSent = document.querySelector('#messageSent') as HTMLElement
+// const inputText = document.querySelectorAll('.inputText') as NodeListOf <HTMLInputElement>
+// document.getElementById('myForm')?.addEventListener('submit', function(event) {
+//     event.preventDefault() // Prevent the default form submission
+//     emailjs.sendForm('service_m5a5vcb', 'template_1jfyvjh', this)
+//     .then(function(response: any) {
+//         messageSent.classList.remove('hide')
+//         inputText.forEach(text => text.value= '')
+//         console.log('SUCCESS!', response.status, response.text)
+//         setTimeout(()=>{ messageSent.classList.add('hide') }, 5000)
+//         // alert("Email sent successfully!")
+//     }, function(error: any) {
+//         console.log('FAILED...', error)
+//         alert("Failed to send email.")
+//     })
+// })
+// console.log('all the time 4 try')
--- chef-kapernaros/script/main.ts
+++ chef-kapernaros/script/main.ts
@@ -1,120 +1,123 @@
-console.log('hello world')
+// console.log('hello world')
 
-const hambMenu = document.querySelector('.hamburger-menu') as HTMLElement
-const navMenu = document.getElementById('navMenu') as HTMLElement
-const navBar = document.querySelector('.navBar') as HTMLElement
-const spanToBreak = document.querySelectorAll('.brSm') as NodeListOf<HTMLElement>
+// const hambMenu = document.querySelector('.hamburger-menu') as HTMLElement
+// const navMenu = document.getElementById('navMenu') as HTMLElement
+// const navBar = document.querySelector('.navBar') as HTMLElement
+// const spanToBreak = document.querySelectorAll('.brSm') as NodeListOf<HTMLElement>
+// if (hambMenu) {
 
-hambMenu.addEventListener('click', function() {
-    navMenu.classList.toggle('show')
-    navMenu.classList.toggle('hide',!navMenu.classList.contains('show'))
-    hambMenu.classList.toggle('openHam') 
-    navBar.classList.toggle('expand')
-})
+//     hambMenu.addEventListener('click', function() {
+//         navMenu.classList.toggle('show')
+//         navMenu.classList.toggle('hide',!navMenu.classList.contains('show'))
+//         hambMenu.classList.toggle('openHam') 
+//         navBar.classList.toggle('expand')
+//     })
+// }
 
-const navBarAdjToScreen = () => {
+
+// const navBarAdjToScreen = () => {
     
-    if (window.innerWidth < 750 && navBar.classList.contains('biggerScreen')) {
+//     if (window.innerWidth < 750 && navBar.classList.contains('biggerScreen')) {
        
-        navBar.classList.add('mobileView')
-        navBar.classList.remove('biggerScreen')
-        spanToBreak.forEach(el => {
-            if (!el.firstChild || el.firstChild.nodeName !== 'BR') {
-                const brEl = document.createElement('br')
-                el.prepend(brEl)
-            }
-        })
-    } else if (window.innerWidth >= 750 && !navBar.classList.contains('biggerScreen')) {
+//         navBar.classList.add('mobileView')
+//         navBar.classList.remove('biggerScreen')
+//         spanToBreak.forEach(el => {
+//             if (!el.firstChild || el.firstChild.nodeName !== 'BR') {
+//                 const brEl = document.createElement('br')
+//                 el.prepend(brEl)
+//             }
+//         })
+//     } else if (window.innerWidth >= 750 && !navBar.classList.contains('biggerScreen')) {
         
-        navBar.classList.remove('mobileView')
-        navBar.classList.add('biggerScreen')
-        spanToBreak.forEach(el => {
-            if (el.firstChild && el.firstChild.nodeName === 'BR') {
-                el.removeChild(el.firstChild)
-            }
-        })
-    }
-}
-const navScrolling = ()=>{
-    if (window.pageYOffset > 30) {
-        navBar.classList.add('scrolled')
-    } else {
-        if (!navBar.classList.contains('scrolledAlways')) {
-            navBar.classList.remove('scrolled')
-        }
-    }
-}
+//         navBar.classList.remove('mobileView')
+//         navBar.classList.add('biggerScreen')
+//         spanToBreak.forEach(el => {
+//             if (el.firstChild && el.firstChild.nodeName === 'BR') {
+//                 el.removeChild(el.firstChild)
+//             }
+//         })
+//     }
+// }
+// const navScrolling = ()=>{
+//     if (window.pageYOffset > 30) {
+//         navBar.classList.add('scrolled')
+//     } else {
+//         if (!navBar.classList.contains('scrolledAlways')) {
+//             navBar.classList.remove('scrolled')
+//         }
+//     }
+// }
 
-const picAnimation = () => {
-    document.querySelectorAll('.bioLandingPicContainer img').forEach(img => {
-        if (window.pageYOffset >= 600) {
-            img.classList.add('in-view')
-        } else if (window.pageYOffset < 500) {
-            img.classList.remove('in-view')
-        }
-    })
-    document.querySelectorAll('.achCards').forEach(img => {
-        if (window.pageYOffset > 1250) {
-            img.classList.add('in-view')
-        } else if (window.pageYOffset < 1150) {
-            img.classList.remove('in-view')
-        }
-    })
-    document.querySelectorAll('.charityLandingPicContainer img').forEach(img => {
-        if (window.pageYOffset >= 1950) {
-            img.classList.add('in-view')
-        } else if (window.pageYOffset < 1850) {
-            img.classList.remove('in-view')
-        }
-    })
-}
+// const picAnimation = () => {
+//     document.querySelectorAll('.bioLandingPicContainer img').forEach(img => {
+//         if (window.pageYOffset >= 600) {
+//             img.classList.add('in-view')
+//         } else if (window.pageYOffset < 500) {
+//             img.classList.remove('in-view')
+//         }
+//     })
+//     document.querySelectorAll('.achCards').forEach(img => {
+//         if (window.pageYOffset > 1250) {
+//             img.classList.add('in-view')
+//         } else if (window.pageYOffset < 1150) {
+//             img.classList.remove('in-view')
+//         }
+//     })
+//     document.querySelectorAll('.charityLandingPicContainer img').forEach(img => {
+//         if (window.pageYOffset >= 1950) {
+//             img.classList.add('in-view')
+//         } else if (window.pageYOffset < 1850) {
+//             img.classList.remove('in-view')
+//         }
+//     })
+// }
 
-window.addEventListener('resize',navBarAdjToScreen)
-navBarAdjToScreen()
+// window.addEventListener('resize',navBarAdjToScreen)
+// navBarAdjToScreen()
 
-window.addEventListener('scroll',() => { 
-    navScrolling() 
-    picAnimation()
-})
-navScrolling()
-picAnimation()
+// window.addEventListener('scroll',() => { 
+//     navScrolling() 
+//     picAnimation()
+// })
+// navScrolling()
+// picAnimation()
 
-const container = document.querySelector('#bigPic') as HTMLDivElement
+// const container = document.querySelector('#bigPic') as HTMLDivElement
 
-let allNotActiveTitles = document.querySelectorAll('.secTitles') as NodeListOf<HTMLLIElement>
-allNotActiveTitles.forEach( listEl => {
-    listEl.addEventListener('click', () => {
-        allNotActiveTitles = document.querySelectorAll('.secTitles') as NodeListOf<HTMLLIElement>
-        console.log(listEl)
+// let allNotActiveTitles = document.querySelectorAll('.secTitles') as NodeListOf<HTMLLIElement>
+// allNotActiveTitles.forEach( listEl => {
+//     listEl.addEventListener('click', () => {
+//         allNotActiveTitles = document.querySelectorAll('.secTitles') as NodeListOf<HTMLLIElement>
+//         console.log(listEl)
 
-        if (!listEl.classList.contains('active')) {
-            const titleActive = [...allNotActiveTitles].filter(el => el.classList.contains('active'))[0]
-            titleActive.classList.remove('active')
-            listEl.classList.add('active')
-        }
-    })
-})
+//         if (!listEl.classList.contains('active')) {
+//             const titleActive = [...allNotActiveTitles].filter(el => el.classList.contains('active'))[0]
+//             titleActive.classList.remove('active')
+//             listEl.classList.add('active')
+//         }
+//     })
+// })
 
-declare var emailjs: any;
-emailjs.init('0wA6kpUaumn2FNdbg')
-const messageSent = document.querySelector('#messageSent') as HTMLElement
-const inputText = document.querySelectorAll('.inputText') as NodeListOf <HTMLInputElement>
+// declare var emailjs: any;
+// emailjs.init('0wA6kpUaumn2FNdbg')
+// const messageSent = document.querySelector('#messageSent') as HTMLElement
+// const inputText = document.querySelectorAll('.inputText') as NodeListOf <HTMLInputElement>
 
 
-document.getElementById('myForm')?.addEventListener('submit', function(event) {
-    event.preventDefault() // Prevent the default form submission
+// document.getElementById('myForm')?.addEventListener('submit', function(event) {
+//     event.preventDefault() // Prevent the default form submission
     
-    emailjs.sendForm('service_m5a5vcb', 'template_1jfyvjh', this)
-    .then(function(response: any) {
-        messageSent.classList.remove('hide')
-        inputText.forEach(text => text.value= '')
-        console.log('SUCCESS!', response.status, response.text)
-        setTimeout(()=>{ messageSent.classList.add('hide') }, 5000)
-        // alert("Email sent successfully!")
-    }, function(error: any) {
-        console.log('FAILED...', error)
-        alert("Failed to send email.")
-    })
-})
+//     emailjs.sendForm('service_m5a5vcb', 'template_1jfyvjh', this)
+//     .then(function(response: any) {
+//         messageSent.classList.remove('hide')
+//         inputText.forEach(text => text.value= '')
+//         console.log('SUCCESS!', response.status, response.text)
+//         setTimeout(()=>{ messageSent.classList.add('hide') }, 5000)
+//         // alert("Email sent successfully!")
+//     }, function(error: any) {
+//         console.log('FAILED...', error)
+//         alert("Failed to send email.")
+//     })
+// })
 
-console.log('all the time 4 try')
+// console.log('all the time 4 try')
